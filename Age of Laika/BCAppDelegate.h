27feef1845c50ec82643f4e4a2714c304a0dd855
@makingthecase <bdc@Brians-MacBook-Air.local>
2014-08-22 18:08:36 -0700

    Initial Commit

---
+++ Age of Laika/BCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCAppDelegate.h
+//  Age of Laika
+//
+//  Created by Brian on 8/22/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Age of Laika/BCViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCViewController.h
+//  Age of Laika
+//
+//  Created by Brian on 8/22/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCViewController : UIViewController
+
+@end
